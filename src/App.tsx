0796600f829b9135fbf1d472c7db3d456f696f83
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-07-30 11:32:46 -0300

    style app margin

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./components/Navbar"
 
 function App() {
     return (
-        <div className="m-12">
+        <div>
             <Navbar/>
             <Outlet/>
         </div>
