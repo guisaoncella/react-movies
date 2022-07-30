fe6f3677753bfa9b279803630bb6ce0892eaba5c
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-07-30 10:35:55 -0300

    add logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Logo } from "./components/Logo"
+
 function App() {
     return (
         <div className="m-12">
-            <div className="bg-cinema-bg-2 text-5xl text-cinema-text-1 font-cursive rounded-full p-8 inline-block">Rmovies</div> 
+            <Logo/> 
         </div>
     )
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export function Logo(){
+    return(
+        <div className="bg-cinema-bg-2 text-5xl text-cinema-text-1 font-cursive rounded-full p-8 inline-block">Rmovies</div>
+    )
+}
