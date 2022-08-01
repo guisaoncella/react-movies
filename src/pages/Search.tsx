944a9d24dab3f062d0d517b8b5d4379b095e70dd
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-08-01 17:32:17 -0300

    feat: search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import { MovieCard } from "../components/MovieCard"
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 export function Search(){
+    const [searchedMovies, setSearchedMovies] = useState<any[]>([]);
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get("q");
+
+    const getSearchedMovies = async (url: string) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setSearchedMovies(data.results);
+    }
+
+    useEffect(() => {
+        const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+
+        getSearchedMovies(searchWithQueryURL);
+    }, [query])
+
     return(
-        <h1>Search</h1>
+        <div>
+            <h2 className="text-4xl text-cinema-text-2 text-center mb-12 font-bold">- Resultados para
+                <span className="uppercase text-white"> {query}</span>
+             -</h2>
+            <div className="flex flex-wrap gap-8 text-white justify-center">
+                {searchedMovies.length === 0 ? <p>Carregando...</p> : 
+                    searchedMovies.map(movie =>{
+                        return(
+                            <MovieCard 
+                                title={movie.title} 
+                                overview={movie.overview} 
+                                poster_path={movie.poster_path} 
+                                vote_average={movie.vote_average} 
+                                id={movie.id}
+                                showLink={true}
+                                key={movie.id} />
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
